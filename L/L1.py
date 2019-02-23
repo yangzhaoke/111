42c74009f2226c2dc3fcfa7ba5beb6b3418ec5c1
lenovo <2495844356@qq.com>
2019-02-23 09:06:58 +0800

    Initial commit

---
+++ L/L1.py
@@ -0,0 +1,26 @@
+# a=open('text.txt','w')
+# a.write('你好啊')
+# a.close()
+# a1=open('text.txt','r')
+# b=a1.readlines()
+# print(b)
+# a1=open('text.txt','r')
+# b=a1.readlines()
+# print(b)
+#
+# a3=open('test1.txt','w',encoding="utf-8")
+# a3.write("%s"%b)
+# a3.close()
+# a=[1,2,3,4,5]
+# import  copy
+# a1=copy.copy(a)
+# print(a1)
+# a1.reverse()
+# print(a1)
+import  random
+import  math
+for i in range(100,201):
+    for j in range(2,int(math.sqrt(i))):
+        if i%j==0
+            b=0
+            break
